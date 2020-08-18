d1536f07a3165839d3f8f8564fbe0335f7a14c6a
alasoui <alaasoui29@gmail.com>
2020-08-18 12:34:48 +0100

    first project

--- main.js
+++ main.js
@@ -33,4 +33,19 @@ $(document).ready(function(){
   	$(".food").show("slow");
   	$(".make").hide();
   });
+
+
+    $("#submit").click(function(){
+      var text = $("#recipe").val();
+      var pic = $("#picture").val()
+      var desc = $("#describe").val();
+      var newdiv = $(`<div class="food"></div>`)
+      
+     $(newdiv).append($(`<h2></h2>`).text(text))
+     $(newdiv).append($(`<img src="${$("#picture").val()}">`))
+     $(newdiv).append($(`<p></p>`).text(desc))
+     $(newdiv).append($(`<img src="${$("#picture").val()}">`))
+      $("body").append(newdiv)
+    })
+
 });
